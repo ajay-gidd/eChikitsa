93d0a53d0dfa88f1ca62ed7a345c8c4c814c6c4f
ajay-gidd <ajay.a.gidd@gmail.com>
2024-04-24 22:54:23 +0530

    registered file handle service to api gateway

--- eChikitsaAPIGateway/src/main/java/com/example/eChikitsaAPIGateway/filter/RouteValidator.java
+++ eChikitsaAPIGateway/src/main/java/com/example/eChikitsaAPIGateway/filter/RouteValidator.java
@@ -10,11 +10,13 @@ import java.util.function.Predicate;
 public class RouteValidator {
 
     public static final List<String> openApiEndpoints = List.of(
+            "/aut/get",
             "/auth/login",
             "/auth/registerPatient",
             "/auth/welcome",
             "/eureka",
             "/user-handle/api/signUp",
+            "/user-handle/auth/contact-us",
             "/user-handle/hospital/add-hospital",
             "/user-handle/auth/login","/user-handle/auth/login-using-otp","/user-handle/patient/registerPatient","/user-handle/admin/registerDoctor", "/user-handle/auth/reset-password-otp","/user-handle/auth/reset-password","/user-handle/email/sendEmail","/user-handle/email/valOtp"
     );
